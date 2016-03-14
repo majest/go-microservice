b5679651bcf336ed11f5ab74639d5954b3a69c23
Artur Grabowski <majest.pl@gmail.com>
2016-03-14 16:12:54 +0000

    Added config to control the ips and ports

--- consul/consul.go
+++ consul/consul.go
@@ -1,73 +1,57 @@
 package consul
 
 import (
-	"errors"
 	"fmt"
-	"net"
 	"os"
 	"os/signal"
 	"syscall"
 
-	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/aws/ec2metadata"
-	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/hashicorp/consul/api"
 	"github.com/satori/go.uuid"
 )
 
 type Consul struct {
 	Client      *api.Client
+	Config      *Config
 	serviceName string
 	UUID        string
 }
 
-var dev string
-var port int = 9090
+func RegisterService(serviceName string, config *Config) *Consul {
 
-func RegisterService(serviceName string) *Consul {
+	if config == nil {
+		config = &Config{}
+	}
+
 	c := &Consul{}
-	c.Init()
+	config.SetDefaults()
+	c.Config = config
+
+	c.init()
 	c.RegisterService(serviceName, uuid.NewV4().String())
 	return c
 }
 
 func New() *Consul {
 	c := &Consul{}
-	c.Init()
+	c.init()
 	return c
 }
 
-func (c *Consul) Init() {
-	dev = os.Getenv("CONSUL_DEV")
-	c.CreateClient()
-	c.HandleExit()
+func (c *Consul) init() {
+	c.createClient()
+	c.handleExit()
 }
 
-func (c *Consul) CreateConfig() *api.Config {
-	nodeIP, err := c.getNodeIP()
+func (c *Consul) createApiConfig() *api.Config {
 
-	if err != nil {
-		panic(err.Error())
-	}
-
-	config := api.DefaultConfig()
-	config.Address = nodeIP + ":8500"
-
-	fmt.Println("Config Address:" + config.Address)
-	return config
+	apiConfig := api.DefaultConfig()
+	apiConfig.Address = fmt.Sprintf("%s:%v", c.Config.NodeIp, c.Config.NodePort)
+	fmt.Println("Consul Address:" + apiConfig.Address)
+	return apiConfig
 }
 
-func (c *Consul) getNodeIP() (string, error) {
-
-	if dev == "docker-local" || dev == "local" {
-		return "192.168.99.101", nil
-	}
-
-	awsmeta := ec2metadata.New(session.New(&aws.Config{Region: aws.String("us-east-1")}))
-	return awsmeta.GetMetadata("local-ipv4")
-}
-
-func (c *Consul) HandleExit() {
+func (c *Consul) handleExit() {
 	signalChannel := make(chan os.Signal, 2)
 	signal.Notify(signalChannel, os.Interrupt, syscall.SIGTERM)
 	go func() {
@@ -85,11 +69,11 @@ func (c *Consul) HandleExit() {
 	}()
 }
 
-func (c *Consul) CreateClient() {
-	client, errc := api.NewClient(c.CreateConfig())
+func (c *Consul) createClient() {
+	client, errc := api.NewClient(c.createApiConfig())
 	if errc != nil {
 		fmt.Println("Error:" + errc.Error())
-		panic(errc.Error())
+		os.Exit(1)
 	}
 	c.Client = client
 }
@@ -98,62 +82,16 @@ func (c *Consul) RegisterService(service, serviceUUID string) {
 	c.UUID = serviceUUID
 	c.serviceName = service
 
-	fmt.Println("Registering service: " + service + ":" + c.UUID)
-	localIP, err := c.getIP()
-
-	if err != nil {
-		panic(err.Error())
-	}
-
-	fmt.Printf("Local ip address: %s:%v\n", localIP, port)
-	err = c.Client.Agent().ServiceRegister(&api.AgentServiceRegistration{
+	fmt.Printf("Service address: %s:%v\n", c.Config.ServiceIp, c.Config.ServicePort)
+	err := c.Client.Agent().ServiceRegister(&api.AgentServiceRegistration{
 		ID:      c.UUID,
 		Name:    service,
-		Port:    port,
-		Address: localIP,
+		Port:    c.Config.ServicePort,
+		Address: c.Config.ServiceIp,
 	})
 
 	if err != nil {
 		fmt.Printf("Error while registering service: %s", err.Error())
 		os.Exit(1)
 	}
-
-}
-
-func (c *Consul) getIP() (string, error) {
-
-	if dev == "local" {
-		return "127.0.0.1", nil
-	}
-
-	ifaces, err := net.Interfaces()
-
-	if err != nil {
-		return "", err
-	}
-
-	var ip net.IP
-
-	for _, i := range ifaces {
-		addrs, err := i.Addrs()
-
-		if err != nil {
-			return "", err
-		}
-
-		for _, addr := range addrs {
-			switch v := addr.(type) {
-			case *net.IPAddr:
-				ip = v.IP
-			case *net.IPNet:
-				ip = v.IP
-			}
-
-			if ip.To4() != nil && ip.String() != "127.0.0.1" {
-				return ip.String(), nil
-			}
-		}
-	}
-
-	return "", errors.New("Could not find local ip address")
 }
